7b4e819961af3b4b52f9330461a82085e860df2e
Amruth L P <134767358+AmruthLP12@users.noreply.github.com>
2024-07-13 20:17:23 +0530

    Flask Login Module

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint , render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -15,6 +16,7 @@ def login():
         if user: 
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
@@ -24,8 +26,10 @@ def login():
     return render_template("login.html" ,boolean = True)
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route("/sign-up" ,methods=['GET', 'POST'])
 def sign_up():
@@ -51,6 +55,7 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1 , method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('Account Created!', category='succes')
             return redirect(url_for('views.home'))
 
